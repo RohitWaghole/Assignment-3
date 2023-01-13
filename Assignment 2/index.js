09ab30f2ca21ec56bbeca9b52f4fd332b9cafb4b
Rohit Waghole <rohitwaghole4@gmail.com>
2023-01-14 01:50:43 +0530

    Adding Documentation to the API using Swagger UI

--- Assignment 2/index.js
+++ Assignment 2/index.js
@@ -1,17 +1,82 @@
-const { response } = require("express");
 const express = require("express");
 const MongoClient = require("mongodb").MongoClient;
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 const app = express();
 
+app.use(express.json());
 var database;
 
-app.use(express.json());
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Node JS API Project Using MongoDB",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/",
+      },
+    ],
+  },
+  apis: ["./index.js"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+app.use("/documentation", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+/**
+ * @swagger
+ * /:
+ *  get:
+ *      summary: Checking if get method working or not
+ *      description: Cheking Purpose
+ *      responses:
+ *        2013:
+ *              description: get method is working
+ *
+ */
 
 // DEFAULT PAGE
 app.get("/", (request, response) => {
-  response.send('<h1><a href="http://localhost:3000/products"/>Products</h1>');
+  response.send(
+    '<h1><a href="http://localhost:3000/products"/>Products</h1><br><h1><a href="http://localhost:3000/documentation"/>Documentation</h1>'
+  );
 });
+
+/**
+ * @swagger
+ *  components:
+ *      schemas:
+ *          Product:
+ *              type : object
+ *              properties:
+ *                  id:
+ *                      type: integer
+ *                  product:
+ *                      type: string
+ *                  price:
+ *                      type: integer
+ */
+
+/**
+ * @swagger
+ * /products:
+ *  get:
+ *      summary: Getting all the products
+ *      description: Get all the products listed down
+ *      responses:
+ *        2013:
+ *              description: Fetch the data from MongoDB
+ *              content:
+ *                    application/json:
+ *                        schema:
+ *                            type: array
+ *                            items:
+ *                                $ref: '#components/schemas/Product'
+ */
 
 // GET ALL THE PRODUCTS
 app.get("/products", (request, response) => {
@@ -24,6 +89,30 @@ app.get("/products", (request, response) => {
     });
 });
 
+/**
+ * @swagger
+ * /products/{product}:
+ *  get:
+ *      summary: Getting product by product name
+ *      description: Get the product by writing down the name of it
+ *      parameters:
+ *          - in: path
+ *            name : product
+ *            required: true
+ *            description: Product name required
+ *            schema:
+ *              type: string
+ *      responses:
+ *        2013:
+ *              description: Fetch the data from MongoDB
+ *              content:
+ *                    application/json:
+ *                        schema:
+ *                            type: array
+ *                            items:
+ *                                $ref: '#components/schemas/Product'
+ */
+
 // GET PRODUCTS BY PRODUCT NAME
 app.get("/products/:product", (request, response) => {
   database
@@ -35,6 +124,31 @@ app.get("/products/:product", (request, response) => {
     });
 });
 
+/**
+ * @swagger
+ * /products_id/{id}:
+ *  get:
+ *      summary: Getting product by product id
+ *      description: Get the product by writing down the id of it
+ *      parameters:
+ *          - in: path
+ *            name : id
+ *            required: true
+ *            description: Numeric ID required
+ *            schema:
+ *              type: integer
+ *      responses:
+ *        2013:
+ *              description: Fetch the data from MongoDB
+ *              content:
+ *                    application/json:
+ *                        schema:
+ *                            type: array
+ *                            items:
+ *                                $ref: '#/components/schemas/Product'
+ *
+ */
+
 // GET PRODUCTS BY PRODUCT ID
 app.get("/products_id/:id", (request, response) => {
   database
@@ -45,6 +159,23 @@ app.get("/products_id/:id", (request, response) => {
       response.send(result);
     });
 });
+
+/**
+ * @swagger
+ * /products/addproduct:
+ *  post:
+ *      summary: Adding new products
+ *      description: Add new product in the MongoDB database
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: "#/components/schemas/Product"
+ *      responses:
+ *        2013:
+ *              description: Added Successfully!
+ */
 
 // ADD/CREATE NEW PRODUCTS
 app.post("/products/addproduct", (request, response) => {
@@ -64,6 +195,36 @@ app.post("/products/addproduct", (request, response) => {
   });
 });
 
+/**
+ * @swagger
+ * /products/{id}:
+ *  put:
+ *      summary: Updating current products
+ *      description: Updating the product in the MongoDB database
+ *      parameters:
+ *          - in: path
+ *            name : id
+ *            required: true
+ *            description: Numeric ID required
+ *            schema:
+ *              type: integer
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: "#/components/schemas/Product"
+ *      responses:
+ *        2013:
+ *              description: Updated Successfully!
+ *              content:
+ *                    application/json:
+ *                        schema:
+ *                            type: array
+ *                            items:
+ *                                $ref: '#/components/schemas/Product'
+ */
+
 // UPDATE THE PRODUCT
 app.put("/products/:id", (request, response) => {
   let query = { id: parseInt(request.params.id) };
@@ -80,6 +241,24 @@ app.put("/products/:id", (request, response) => {
     response.send(data);
   });
 });
+
+/**
+ * @swagger
+ * /products/{id}:
+ *  delete:
+ *      summary: Deleting current products
+ *      description: Deleting the product in the MongoDB database
+ *      parameters:
+ *          - in: path
+ *            name : id
+ *            required: true
+ *            description: Numeric ID required
+ *            schema:
+ *              type: integer
+ *      responses:
+ *        2013:
+ *              description: Deleted Successfully!
+ */
 
 // DELETE THE PRODUCT
 app.delete("/products/:id", (request, response) => {
